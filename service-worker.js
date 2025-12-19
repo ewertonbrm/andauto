fe2a28a3fa80723bbe7b374ddef367759ee7cb96
Ewerton Melo <88634234+ewertonbrm@users.noreply.github.com>
2025-12-19 10:10:26 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,34 @@
-const CACHE_NAME = 'android-authority-pwa-cache-v1';
-const urlsToCache = [
-    '/androidauthority/',
-    '/androidauthority/index.html',
-    '/androidauthority/manifest.json',
-    '/androidauthority/icons/icon-192x192.png',
-    '/androidauthority/icons/icon-512x512.png',
-    '/androidauthority/icons/loading.gif' // <-- GIF de carregamento adicionado aqui
+const CACHE_NAME = 'aauthority-teste-v12';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icons/loading.gif',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Service Worker: Arquivos críticos cacheados.');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
